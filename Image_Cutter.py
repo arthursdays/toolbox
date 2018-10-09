92364866b6a876f3a42576477f95c3419b5d7374
arthursdays <justinwangshihao@gmail.com>
2018-10-09 17:28:05 +0800

    Able to deal with blocks of points

--- Image_Cutter.py
+++ Image_Cutter.py
@@ -34,47 +34,79 @@ def cut_image(line):
 
     for record in parse_info.split('],["'):
         # print("{}: {}_{}".format(img_name, record, record.count('],[')))
-        if record.count('],[') == 1:
-            print(record)
+        # if record.count('],[') == 1:
+        #     print(record)
+        #     state = record.split('"",')[0].split('"')[-1]
+        #     x = (int)(record.split('[[')[1].split(',')[0])
+        #     y = (int)(record.split('],[')[0].split(',')[-1])
+        #     w = (int)((float)(record.split('],[')[1].split(',')[0]))
+        #     h = (int)((float)(record.split(']]')[0].split(',')[-1]))
+        #     print("Saving {}...".format(os.path.join("..\\yinxiebing\\cut_images",
+        #                                         "{}_{}_{}_{},{}_{},{}.jpg".format(
+        #                                             img_name.split('.jpg')[0],
+        #                                             state,
+        #                                             dr_id,
+        #                                             x,
+        #                                             y,
+        #                                             w,
+        #                                             h))))
+        #
+        #     cv2.imwrite(os.path.join("..\\yinxiebing\\cut_images",
+        #                         "{}_{}_{}_{},{}_{},{}.jpg".format(
+        #                             img_name.split('.jpg')[0],
+        #                             state,
+        #                             dr_id,
+        #                             x,
+        #                             y,
+        #                             w,
+        #                             h)),
+        #                 img[y:y+h, x:x+w])
+        if record.count('],[') > 1:
+            record_list = record.split(',[[')[1].split(']],')[0].split('],[')
+            # print(record_list)
             state = record.split('"",')[0].split('"')[-1]
-            x = (int)(record.split('[[')[1].split(',')[0])
-            y = (int)(record.split('],[')[0].split(',')[-1])
-            w = (int)((float)(record.split('],[')[1].split(',')[0]))
-            h = (int)((float)(record.split(']]')[0].split(',')[-1]))
-            print("Saving {}...".format(os.path.join("..\\yinxiebing\\cut_images",
-                                                "{}_{}_{}_{},{}_{},{}.jpg".format(
-                                                    img_name.split('.jpg')[0],
-                                                    state,
-                                                    dr_id,
-                                                    x,
-                                                    y,
-                                                    w,
-                                                    h))))
+            min_x = (int)(record_list[0].split(',')[0])
+            min_y = (int)(record_list[0].split(',')[1])
+            max_x = (int)(record_list[0].split(',')[0])
+            max_y = (int)(record_list[0].split(',')[1])
+            for point in record_list:
+                min_x = (int)(point.split(',')[0]) if ((int)(point.split(',')[0]) < min_x) else min_x
+                min_y = (int)(point.split(',')[1]) if ((int)(point.split(',')[1]) < min_y) else min_y
+                max_x = (int)(point.split(',')[0]) if ((int)(point.split(',')[0]) > max_x) else max_x
+                max_y = (int)(point.split(',')[1]) if ((int)(point.split(',')[1]) > max_y) else max_y
+            # print('[{},{}],[{},{}]'.format(min_x, min_y, max_x, max_y))
+            print("Saving {}...".format(os.path.join("..\\yinxiebing\\cutted_images",
+                                                    "{}_{}_{}_{},{}_{},{}.jpg".format(
+                                                        img_name.split('.jpg')[0],
+                                                        state,
+                                                        dr_id,
+                                                        min_x,
+                                                        min_y,
+                                                        max_x - min_x,
+                                                        max_y - min_y))))
 
-            cv2.imwrite(os.path.join("..\\yinxiebing\\cut_images",
-                                "{}_{}_{}_{},{}_{},{}.jpg".format(
-                                    img_name.split('.jpg')[0],
-                                    state,
-                                    dr_id,
-                                    x,
-                                    y,
-                                    w,
-                                    h)),
-                        img[y:y+h, x:x+w])
-
-
+            cv2.imwrite(os.path.join("..\\yinxiebing\\cutted_images",
+                                    "{}_{}_{}_{},{}_{},{}.jpg".format(
+                                        img_name.split('.jpg')[0],
+                                        state,
+                                        dr_id,
+                                        min_x,
+                                        min_y,
+                                        max_x - min_x,
+                                        max_y - min_y)),
+                            img[min_y:max_y, min_x:max_x])
 
 if __name__ == '__main__':
     print("Parent process {}".format(os.getpid()))
     p = multiprocessing.Pool(24)
 
     for line in file.readlines():
-        p.apply_async(cut_image, args=(line, ))
-        # cut_image(line)
+        # p.apply_async(cut_image, args=(line, ))
+        cut_image(line)
     print("Waiting all subprocesses done...")
 
-    p.close()
-    p.join()
+    # p.close()
+    # p.join()
     print("All subprocesses done!")
 
 
